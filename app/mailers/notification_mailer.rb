b586321546ed00f153a651a7f4d129ce742c2669
Ernie Jamison <jamisonernie@gmail.com>
2016-06-15 23:23:51 +0000

    Changed brand font and more CSS styling

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,7 +1,7 @@
 class NotificationMailer < ActionMailer::Base
   default from: "no-reply@nomsterapp.com"
 
-  def comment_added
+  def comment_added(comment)
     mail(to: "jamisonernie@gmail.com", subject: "A comment has been added to your place")
   end
 end
