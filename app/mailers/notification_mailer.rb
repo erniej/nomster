3cfc6066bb58f19c28419ad7530484d5921bf2b0
Ernie Jamison <jamisonernie@gmail.com>
2016-06-16 11:25:11 +0000

    Adjusted automated emailer mailer

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -2,6 +2,9 @@ class NotificationMailer < ActionMailer::Base
   default from: "no-reply@nomsterapp.com"
 
   def comment_added(comment)
-    mail(to: "jamisonernie@gmail.com", subject: "A comment has been added to your place")
+    @place = comment.place
+    @place_owner = @place.user
+
+    mail(to: @place_owner.email, subject: "A comment has been added to #{@place.name}")
   end
 end
